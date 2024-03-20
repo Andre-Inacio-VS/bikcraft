54a078491d03036e5b25d450a36cd83bb623e825
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-03-20 11:16:07 -0300

    feat (geral): finalizacao do JavaScript do site"

--- js/script.js
+++ js/script.js
@@ -7,23 +7,65 @@ function activeLink(item) {
  if (urlAtual.includes(link)) {
   item.classList.add('active');
  }
-}
+};
 
 links.forEach(activeLink);
 // Links Menu
 
 // Itens Orcamento
-
 const params = new URLSearchParams(location.search);
-console.log(params)
 
 function activeProdutos(parametro) {
- const element = document.getElementById(parametro)
+ const element = document.getElementById(parametro);
  if (element) {
-  element.checked = true
+  element.checked = true;
  }
+};
+
+params.forEach(activeProdutos);
+// Itens Orcamento
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function activePergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle('active');
+    const ativa = resposta.classList.contains('active');
+    pergunta.setAttribute('aria-expanded', ativa);
 }
 
-params.forEach(activeProdutos)
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', activePergunta);
+}
+perguntas.forEach(eventosPerguntas);
+// Perguntas Frequentes
 
-// Itens Orcamento
+// Galeria Bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    
+    const media = matchMedia('(min-width: 934px').matches;
+    if(media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria)
+// Galeria Bicicletas
+
+// Animacao
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
+// Animacao
